dd6e41f6afa89d6edc5ab7812a93b7e9109b5e23
MINBEOM KIM <meanstigerkim@gmail.com>
2023-11-08 15:26:35 +0000

    Groom.ide 연결 및 테스트

---
+++ DoITAlgorithm/DoITJava_000.java
@@ -0,0 +1,9 @@
+public class DoITJava_000 {
+    public static void main(String[] args){
+        int N = 100000;
+        int cnt = 0;
+        for(int i = 0; i < N; i++){
+            System.out.println("연산 횟수:" + cnt++);
+        }
+    }
+}
